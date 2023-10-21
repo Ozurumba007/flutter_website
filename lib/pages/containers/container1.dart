9a0092ceef7407de85f0d61cd1df108edf3b5956
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-10-21 11:22:48 +0100

    created the mobile version for the web app configuring the desktop code

--- lib/pages/containers/container1.dart
+++ lib/pages/containers/container1.dart
@@ -16,11 +16,74 @@ class _Container1State extends State<Container1> {
   @override
   Widget build(BuildContext context) {
     return ScreenTypeLayout.builder(
-      mobile: (BuildContext context) => DesktopContainer1(),
+      mobile: (BuildContext context) => MobileContainer1(),
       desktop: (BuildContext context) => DesktopContainer1(),
     );
   }
 
+  // ============== M O B I L E ===========
+
+  Widget MobileContainer1() {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            height: w! / 1.2,
+            width: w! / 1.2,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(illustration1),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Track your \nExpenses to \nSave Money',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.hindSiliguri(
+              fontSize: w! / 10,
+              fontWeight: FontWeight.bold,
+              height: 1.2,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Helps you to organize your income and expenses',
+            textAlign: TextAlign.center,
+            style: GoogleFonts.hindSiliguri(
+              color: Colors.grey.shade400,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 30),
+          Container(
+            height: 45,
+            child: ElevatedButton.icon(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(
+                  AppColors.primary,
+                ),
+              ),
+              onPressed: () {},
+              icon: Icon(Icons.arrow_drop_down),
+              label: Text('Try Free Demo'),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            '- Web, IOS and Android',
+            style: GoogleFonts.hindSiliguri(
+              color: Colors.grey.shade400,
+              fontSize: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // ============== DESKTOP ===========
   Widget DesktopContainer1() {
     return Container(
       margin: EdgeInsets.symmetric(
@@ -42,15 +105,15 @@ class _Container1State extends State<Container1> {
                       height: 1.2,
                     ),
                   ),
-                  SizedBox(height: 20),
+                  SizedBox(height: 10),
                   Text(
                     'Helps you to organize your income and expenses',
                     style: GoogleFonts.hindSiliguri(
                       color: Colors.grey.shade400,
-                      fontSize: 16,
+                      fontSize: 20,
                     ),
                   ),
-                  SizedBox(height: 20),
+                  SizedBox(height: 30),
                   Row(
                     children: [
                       Container(
@@ -71,7 +134,7 @@ class _Container1State extends State<Container1> {
                         '- Web, IOS and Android',
                         style: GoogleFonts.hindSiliguri(
                           color: Colors.grey.shade400,
-                          fontSize: 16,
+                          fontSize: 20,
                         ),
                       ),
                     ],
